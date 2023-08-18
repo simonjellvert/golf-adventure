713e09c3a3bb160ed5e4724261de96a2ea230b65
simonjellvert <>
2023-08-18 14:03:44 +0000

    Initial commit, add basic structure to run.py

--- run.py
+++ run.py
@@ -1,3 +1,16 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+def welcome_message()
+
+
+def input_name()
+
+
+def access_rules()
+
+
+def access_leaderboard()
+
+
+def start_game()
+
+
+def main ()
