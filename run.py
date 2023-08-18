18d3fcea6f1a1c269d727d1e2cf11bcffbb27d3c
simonjellvert <>
2023-08-18 15:04:50 +0000

    Add some code to welcome message

--- run.py
+++ run.py
@@ -1,16 +1,38 @@
-def welcome_message()
+name = input("Type your name: ")
+
+class welcome_message():
+    """
+    Function to welcome the player to the game
+    """
+
+    def __init__(self, name):
+        self.name = name
+
+        return 
+
+    print(f"Welcome {name} to this game called Random Round of Golf.\n")
+    print("")
 
 
-def input_name()
+# def input_name():
+    # """
+    # Function to input name
+    # """
 
 
-def access_rules()
+# def access_rules():
+    # """
+    # Function to access rules and how the game is played
+    # """
 
 
-def access_leaderboard()
+# def access_leaderboard():
+    # """
+    # Function to access leaderbord with the top 3 scores all time
+    # """
 
 
-def start_game()
-
-
-def main ()
+# def main():
+    # """
+    # Runs the game
+    # """
